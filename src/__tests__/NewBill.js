09ed32358c9adb7c320d4711bffc237c19df54d7
AdrienM2b <adrien.marchetti.6@gmail.com>
2023-12-05 12:34:10 +0100

    correct test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -13,10 +13,13 @@ import Router from '../app/Router.js';
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on NewBill Page', () => {
-    test('Then ...', () => {
+    test('Then it should display a form', () => {
       const html = NewBillUI();
       document.body.innerHTML = html;
       // to-do write assertion
+      const newBillForm = screen.getByTestId('form-new-bill');
+
+      expect(newBillForm).toBeTruthy();
     });
     describe('When I upload a file', () => {
       beforeEach(() => {
